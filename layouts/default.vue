c223f30a73196866166e0b8e068f4a65f593fb90
fangjie <857516512@qq.com>
2024-02-23 18:16:40 +0800

    初始化nuxt项目

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="layout">
+		<app-header></app-header>
+		<navbar :navbars="navbars"></navbar>
+		<slot></slot>
+		<app-footer></app-footer>
+	</div>
+</template>
+
+<script setup lang="ts">
+	import { getHomeInfo } from "@/service/home";
+	const { data } = await getHomeInfo("oppo");
+	console.log(data.value?.data.navbars);
+	import { useHomeStore } from "@/store/home";
+	import { storeToRefs } from "pinia";
+	const homeStore = useHomeStore();
+	const { navbars } = storeToRefs(homeStore);
+	homeStore.fetchHomeInfoData("oppo"); // 触发了一个 action
+</script>
+         
+<style lang="scss" scoped></style>
---
+++ pages/[...slug].vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="page-not-found">page-not-found : 400</div>
+</template>
+
+<script lang="ts" setup>
+	export interface IProps {
+		title: string;
+	}
+	const props = withDefaults(defineProps<IProps>(), {
+		title: "",
+	});
+</script>
+
+<style lang="scss" scoped></style>
